e1683930857dd27e017c6e3284696fdb1a38d4b0
Joey <bernardinij@outlook.com>
2024-09-26 19:44:00 -0500

    9/26

--- app/src/main/java/com/example/rinkdepth2/ui/proRink/HomeFragment.java
+++ app/src/main/java/com/example/rinkdepth2/ui/proRink/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.rinkdepth2.ui.proRink;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,20 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.rinkdepth2.R;
 import com.example.rinkdepth2.databinding.FragmentHomeBinding;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
@@ -33,23 +48,98 @@ public class HomeFragment extends Fragment {
         CalendarView datePicker;
         EditText et;
         Button viewButton;
+        TextView response;
+
+        String path = "192.168.1.8:8080/C:\\Users\\berna\\Documents\\rinkDepth";
+        Context cont = root.getContext();
+        File file = new File(cont.getFilesDir(), "test2.txt");
+        String data = "Hello World!";
 
         rinkImg = root.findViewById(R.id.proRinkImg);
         rinkImg.setImageResource(R.drawable.prorink);
         datePicker = root.findViewById(R.id.datePickerView);
         et = root.findViewById(R.id.editTextDate);
         viewButton = root.findViewById(R.id.viewBtn);
+        response = root.findViewById(R.id.textView);
 
         datePicker.setOnDateChangeListener((calendarView, year, month, day) -> {
             String date = day + " / " + month + " / " + year;
             et.setText(date);
         });
 
-        viewButton.setOnClickListener(view -> {
-            //TODO: put code here
+        viewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String res = String.valueOf(createFile(cont, file, path, data));
+                response.setText(res);
+            }
         });
 
         return root;
+    }
+
+    private int createFile(Context cont, File file, String path, String data)
+    {
+        boolean newFileCreated = false;
+        try
+        {
+            File newFile = new File(cont.getFilesDir(), file.getName());
+            if (!newFile.createNewFile()) {
+                newFileCreated = false;
+            } else {
+                newFileCreated = true;
+            }
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return writeToFile(cont,file, path, data);
+    }
+
+    private int writeToFile(Context cont, File file, String path, String data)
+    {
+        try (FileOutputStream fos = cont.openFileOutput(file.getName(), Context.MODE_PRIVATE))
+        {
+            fos.write(data.getBytes());
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return uploadDocument(cont, file, path, data);
+    }
+
+    private int uploadDocument(Context cont, File file, String path, String data)
+    {
+        int response = 0;
+        URLConnection urlConn = null;
+
+        try
+        {
+            URL url = new URL(path);
+            urlConn = url.openConnection();
+            urlConn.setDoOutput(true);
+            if (urlConn instanceof HttpURLConnection)
+            {
+                ((HttpURLConnection) urlConn).setRequestMethod("PUT");
+                urlConn.setRequestProperty("Content-type", "text/plain");
+                urlConn.connect();
+            }
+            BufferedOutputStream bos = new BufferedOutputStream(urlConn.getOutputStream());
+            BufferedInputStream bis = new BufferedInputStream(Files.newInputStream(file.toPath()));
+
+            int x;
+            while((x = bis.read()) >-1)
+            {
+                bos.write(x);
+            }
+            bos.close();
+            bis.close();
+            response = ((HttpURLConnection)urlConn).getResponseCode();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return response;
     }
 
     @Override
