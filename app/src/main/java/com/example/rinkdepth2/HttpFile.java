33629061162c1a71971dc8a3faad7697a83aa215
Joey <bernardinij@outlook.com>
2024-10-01 19:00:47 -0500

    10/01

---
+++ app/src/main/java/com/example/rinkdepth2/HttpFile.java
@@ -0,0 +1,114 @@
+package com.example.rinkdepth2;
+
+import android.content.Context;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+
+public class HttpFile
+{
+    public boolean checkConnection(URLConnection urlConn)
+    {
+        try
+        {
+            urlConn.setDoOutput(urlConn.getDoOutput());
+            return false;
+        }catch (IllegalStateException e)
+        {
+            return true;
+        }
+    }
+
+    public int uploadDocument(Context cont, File file, String path, String data)
+    {
+        int response = 0;
+        URLConnection urlConn = null;
+        try
+        {
+            URL url = new URL(path);
+            urlConn = url.openConnection();
+            urlConn.setDoOutput(true);
+            boolean connected = checkConnection(urlConn);
+            if (connected)
+            {
+                if (urlConn instanceof HttpURLConnection)
+                {
+                    ((HttpURLConnection) urlConn).setRequestMethod("POST");
+                    urlConn.setRequestProperty("Content-type", "text/plain");
+                    urlConn.setRequestProperty("Connection", "close");
+                    urlConn.setRequestProperty("http.keepAlive", "false");
+                    urlConn.connect();
+                }
+            }
+            BufferedOutputStream bos = new BufferedOutputStream(urlConn.getOutputStream());
+            BufferedInputStream bis = new BufferedInputStream(Files.newInputStream(file.toPath()));
+
+            int x;
+            while((x = bis.read()) >-1)
+            {
+                bos.write(x);
+            }
+            bos.close();
+            bis.close();
+            response = ((HttpURLConnection)urlConn).getResponseCode();
+            ((HttpURLConnection) urlConn).disconnect();
+            int y = 0;
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    public int createFile(Context cont, File file, String path, String data)
+    {
+        boolean newFileCreated = false;
+        try
+        {
+            File newFile = new File(cont.getFilesDir(), file.getName());
+            if (!newFile.createNewFile()) {
+                newFileCreated = false;
+                //appendToFile(cont, file, path, data);
+            } else {
+                newFileCreated = true;
+            }
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return writeToFile(cont,file, path, data);
+    }
+
+    public int writeToFile(Context cont, File file, String path, String data)
+    {
+//        try (FileOutputStream fos = cont.openFileOutput(file.getName(), Context.MODE_PRIVATE))
+//        {
+//            fos.write(data.toByteArray());
+//        }catch (IOException e)
+//        {
+//            e.printStackTrace();
+//        }
+        try
+        {
+            FileWriter writer = new FileWriter(file);
+            writer.write(data);
+            writer.close();
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return uploadDocument(cont, file, path, data);
+    }
+
+    public int appendToFile(Context cont, File file, String path, String data)
+    {
+        return uploadDocument(cont, file, path, data);
+    }
+}
--- app/src/main/java/com/example/rinkdepth2/ui/proRink/HomeFragment.java
+++ app/src/main/java/com/example/rinkdepth2/ui/proRink/HomeFragment.java
@@ -15,23 +15,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.rinkdepth2.HttpFile;
 import com.example.rinkdepth2.R;
 import com.example.rinkdepth2.databinding.FragmentHomeBinding;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.Files;
-import java.io.FileWriter;
 
 public class HomeFragment extends Fragment {
 
@@ -45,13 +33,14 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        HttpFile httpFile = new HttpFile();
         ImageView rinkImg;
         CalendarView datePicker;
         EditText et;
         Button viewButton;
         TextView response;
 
-        String path = "http://192.168.1.8:8080/C:\\Users\\berna\\Documents\\rinkDepth";
+        String path = "http://192.168.1.8:8080";
         Context cont = root.getContext();
         File file = new File(cont.getFilesDir(), "test.txt");
         String data = "Hello World!";
@@ -72,97 +61,13 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 new Thread(() -> {
-                    String res = String.valueOf(createFile(cont, file, path, data));
+                    String res = String.valueOf(httpFile.createFile(cont, file, path, data));
                     response.setText(res);
                 }).start();
             }
         });
 
         return root;
-    }
-
-    private int createFile(Context cont, File file, String path, String data)
-    {
-        boolean newFileCreated = false;
-        try
-        {
-            File newFile = new File(cont.getFilesDir(), file.getName());
-            if (!newFile.createNewFile()) {
-                newFileCreated = false;
-                //appendToFile(cont, file, path, data);
-            } else {
-                newFileCreated = true;
-            }
-        }catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        return writeToFile(cont,file, path, data);
-    }
-
-    private int writeToFile(Context cont, File file, String path, String data)
-    {
-//        try (FileOutputStream fos = cont.openFileOutput(file.getName(), Context.MODE_PRIVATE))
-//        {
-//            fos.write(data.toByteArray());
-//        }catch (IOException e)
-//        {
-//            e.printStackTrace();
-//        }
-        try
-        {
-            FileWriter writer = new FileWriter(file);
-            writer.write(data);
-            writer.close();
-        }catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        return uploadDocument(cont, file, path, data);
-    }
-
-    private int appendToFile(Context cont, File file, String path, String data)
-    {
-        return uploadDocument(cont, file, path, data);
-    }
-
-    private int uploadDocument(Context cont, File file, String path, String data)
-    {
-        int response = 0;
-        URLConnection urlConn = null;
-
-        try
-        {
-            URL url = new URL(path);
-            //URL url = new URL("https", "192.168.1.8", 8080, "C:\\Users\\berna\\Documents\\rinkDepth\\test.txt");
-            urlConn = url.openConnection();
-            urlConn.setDoOutput(true);
-            if (urlConn instanceof HttpURLConnection)
-            {
-                ((HttpURLConnection) urlConn).setRequestMethod("PUT");
-                urlConn.setRequestProperty("Content-type", "text/plain");
-                urlConn.setRequestProperty("Connection", "close");
-                urlConn.connect();
-            }
-            BufferedOutputStream bos = new BufferedOutputStream(urlConn.getOutputStream());
-            BufferedInputStream bis = new BufferedInputStream(Files.newInputStream(file.toPath()));
-
-            int x;
-            while((x = bis.read()) >-1)
-            {
-                bos.write(x);
-            }
-            bos.close();
-            bis.close();
-            assert urlConn instanceof HttpURLConnection;
-            response = ((HttpURLConnection)urlConn).getResponseCode();
-            ((HttpURLConnection) urlConn).disconnect();
-            int y = 0;
-        }catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        return response;
     }
 
     @Override
